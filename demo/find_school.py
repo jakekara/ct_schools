2fd00088cd102a32320b2f4ed5d6f1684096bf3e
Jake Kara <jake@jakekara.com>
2018-02-23 17:14:18 -0500

    Updated demo to print JSON

--- demo/find_school.py
+++ demo/find_school.py
@@ -1,10 +1,16 @@
 
 import ct_schools.schools as schools
+import json
+# print schools.school_by_name("Shepau Valley", closest=True)
 
-print schools.schooldf().head(1)
+# print schools.schooldf().head(1)
 
-print schools.school_by_name("Shepaug")
+# print schools.school_by_name("Shepaug")
 
-print schools.school_by_name("Shepau Valley", closest=True)
+print json.dumps(
+    json.loads(
+        schools.school_by_name("Shepau Valley", closest=True).to_json(orient="records")
+        ), indent=2
+    )
 
 
